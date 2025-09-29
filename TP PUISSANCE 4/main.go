06b12715690ec6c904f5c76def6bea3daae95cd0
Amir <mohandarezki.amir@ynov.com>
2025-09-29 14:00:29 +0200

    feat: ajout de la fonction init game

---
+++ TP PUISSANCE 4/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Game struct {
+	player01      string
+	player02      string
+	grille        [6][7]string
+	currentPlayer string
+	nbrtour       int
+}
+
+func initGame() Game {
+	fmt.Println("========== BIENVENUE DANS LE PUISSANCE 4 ===========")
+	fmt.Println("LE JOUEUR 1 AURA LES X, LE JOUEUR 2 AURA LES O")
+
+	var grille [6][7]string
+	for i := range grille {
+		for j := range grille[i] {
+			grille[i][j] = " "
+		}
+	}
+
+	return Game{
+		player01:      "X",
+		player02:      "O",
+		grille:        grille,
+		currentPlayer: "X",
+		nbrtour:       0,
+	}
+}
